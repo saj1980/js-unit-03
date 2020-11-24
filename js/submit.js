7d42f45bc1e1c58e2ed5a0a0c195cf8e96cdab0a
saj1980 <sajid.denmark@gmail.com>
2020-11-24 23:24:12 +0100

    More cleaning

--- js/submit.js
+++ js/submit.js
@@ -2,30 +2,14 @@ const error = document.querySelectorAll('.error-border');
 const submit = document.querySelector('button[type="submit"]');
 const asterisk = document.querySelectorAll('.asterisk');
 
-// for(let i = 0; i < asterisk.length; i++){
-//     console.log(asterisk[i])
-// }
-
-
 submit.addEventListener('click', function(e) {
     e.preventDefault();
-
-
     Object.keys(error).forEach(function(i) {
-        console.log('Value '+error[i].value);
-            
             if(error[i].value == ''){
                 error[i].classList.add("error");
                 //asterisk[i].classList.add("red");
                 //error[i].parentElement.classList.add("error");
                 //console.log(asterisk[i]);
             }  
-        
-         
-
-       
     });
-
-    
-
 })
